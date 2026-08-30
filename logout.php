050e51f7e46d974ffdd7dfcff6321c8d19debd6a
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-30 14:10:58 -0600

    Add logout functionality in logout.php

---
+++ logout.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/app/bootstrap.php';
+
+auth_logout();
+
+header('Location: /login.php');
+exit;<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/app/bootstrap.php';
+
+auth_logout();
+
+header('Location: /login.php');
+exit;
