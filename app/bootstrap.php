dbd5a816b383d8999af84be14d2ad9d78698a882
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-30 16:58:57 -0600

    Add weather.php to bootstrap dependencies

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -13,5 +13,6 @@ require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/places.php';
 require_once __DIR__ . '/access.php';
+require_once __DIR__ . '/weather.php';
 
 start_llama_session();
