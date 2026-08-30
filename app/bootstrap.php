679ce4ea1986f3a06664e229adb75a7cf3eafd0e
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-30 16:32:59 -0600

    Implement cache control headers in bootstrap.php
    Add cache control headers if not already sent

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+if (!headers_sent()) {
+    header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+    header('Cache-Control: post-check=0, pre-check=0', false);
+    header('Pragma: no-cache');
+    header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
+}
+
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/places.php';
