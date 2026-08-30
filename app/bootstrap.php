1ef95232faa2aeec2853a20afff3d5bfc67e8cb6
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-08-30 16:24:24 -0600

    Add access.php to bootstrap file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,5 +5,6 @@ declare(strict_types=1);
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/places.php';
+require_once __DIR__ . '/access.php';
 
 start_llama_session();
