51ece952093fadae13b3c872c7684bd8876629ca
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-09-11 03:53:16 -0600

    Add files via upload

--- js/admin/moderate-submission.js
+++ js/admin/moderate-submission.js
@@ -1,1 +1,31 @@
+(() => {
+    'use strict';
 
+    const button =
+        document.querySelector(
+            '[data-delete-submission]'
+        );
+
+    if (!button) {
+        return;
+    }
+
+    let armed = false;
+
+    button.addEventListener(
+        'click',
+        (event) => {
+            if (armed) {
+                return;
+            }
+
+            event.preventDefault();
+
+            armed = true;
+
+            button.classList.add(
+                'is-armed'
+            );
+        }
+    );
+})();
