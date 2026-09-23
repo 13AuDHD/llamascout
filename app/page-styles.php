b126c976ba231ab0d8b4260f72f888af3e1388cf
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-09-23 14:10:19 -0600

    Add files via upload

--- app/page-styles.php
+++ app/page-styles.php
@@ -78,6 +78,7 @@ function llama_page_styles(string $scriptName = ''): array
 
     if (!$isAccount && $basename === 'place.php') {
         $styles[] = 'place-detail.css';
+        $styles[] = 'place-actions.css';
         $styles[] = 'site/features/place-shared.css';
         $styles[] = 'contributor-attribution.css';
         $styles[] = 'share.css';
