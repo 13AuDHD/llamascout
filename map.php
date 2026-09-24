967027beca2943c7fc235df09c825f052a3bb667
thirteen <6532239+13AuDHD@users.noreply.github.com>
2026-09-24 01:24:38 -0600

    Add map-land-overlays.js script to map.php

--- map.php
+++ map.php
@@ -298,5 +298,6 @@ require __DIR__ . '/partials/header.php';
 
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
 <script src="/js/map.js"></script>
+<script src="/js/map-land-overlays.js"></script>
 
 <?php require __DIR__ . '/partials/footer.php'; ?>
